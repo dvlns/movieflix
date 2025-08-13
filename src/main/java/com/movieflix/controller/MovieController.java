c41a3765d743c4e58fb0d738031c67d9a99c5fae
dvlns <davi.carnauba@gmail.com>
2025-08-13 14:29:05 -0300

    feat: add movie entity with repository, service, mapper and controller

---
+++ src/main/java/com/movieflix/controller/MovieController.java
@@ -0,0 +1,73 @@
+package com.movieflix.controller;
+
+import com.movieflix.controller.request.MovieRequest;
+import com.movieflix.controller.response.MovieResponse;
+import com.movieflix.entity.Movie;
+import com.movieflix.mapper.MovieMapper;
+import com.movieflix.service.MovieService;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/movieflix/movie")
+@RequiredArgsConstructor
+public class MovieController {
+    private final MovieService movieService;
+
+    @PostMapping
+    public ResponseEntity<MovieResponse> save(@RequestBody MovieRequest request){
+        Movie savedMovie = movieService.save(MovieMapper.toMovie(request));
+        return ResponseEntity.ok(MovieMapper.toMovieResponse(savedMovie));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MovieResponse>> findAll(){
+        return ResponseEntity.ok(movieService.findAll()
+                .stream()
+                .map(MovieMapper::toMovieResponse)
+                .toList());
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MovieResponse> findById(@PathVariable Long id){
+        return movieService.findById(id)
+                .map(movie -> ResponseEntity.ok(MovieMapper.toMovieResponse(movie)))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MovieResponse> update(@PathVariable Long id, @RequestBody MovieRequest request){
+        return movieService.update(id, MovieMapper.toMovie(request))
+                .map(movie -> ResponseEntity.ok(MovieMapper.toMovieResponse(movie)))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<MovieResponse>> findByCategory(@RequestParam Long category){
+        return ResponseEntity.ok(movieService.findByCategory(category)
+                .stream()
+                .map(MovieMapper::toMovieResponse)
+                .toList());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        Optional<Movie> optionalMovie = movieService.findById(id);
+        if(optionalMovie.isPresent()){
+            movieService.delete(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+
+
+
+}
---
+++ src/main/java/com/movieflix/service/MovieService.java
@@ -0,0 +1,84 @@
+package com.movieflix.service;
+
+import com.movieflix.entity.Category;
+import com.movieflix.entity.Movie;
+import com.movieflix.entity.Streaming;
+import com.movieflix.repository.MovieRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MovieService {
+    private final MovieRepository repository;
+    private final CategoryService categoryService;
+    private final StreamingService streamingService;
+
+    public Movie save(Movie movie){
+        movie.setCategories(this.findCategories(movie.getCategories()));
+        movie.setStreamings(this.findStreamings(movie.getStreamings()));
+        return repository.save(movie);
+    }
+
+    public List<Movie> findAll(){
+        return repository.findAll();
+    }
+
+    public Optional<Movie> findById(Long id){
+        return repository.findById(id);
+    }
+
+    public List<Movie> findByCategory(Long categoryId){
+        return repository.findMovieByCategories(List.of(Category.builder().id(categoryId).build()));
+    }
+
+    public Optional<Movie> update(Long movieId, Movie updateMovie){
+        Optional<Movie> optMovie = repository.findById(movieId);
+        if(optMovie.isPresent()){
+
+            List<Category> categories = this.findCategories(updateMovie.getCategories());
+            List<Streaming> streamings = this.findStreamings(updateMovie.getStreamings());
+
+            Movie movie = optMovie.get();
+            movie.setTitle(updateMovie.getTitle());
+            movie.setDescription(updateMovie.getDescription());
+            movie.setReleaseDate(updateMovie.getReleaseDate());
+            movie.setRating(updateMovie.getRating());
+
+            movie.getCategories().clear();
+            movie.getCategories().addAll(categories);
+
+            movie.getStreamings().clear();
+            movie.getStreamings().addAll(streamings);
+
+            repository.save(movie);
+            return Optional.of(movie);
+        }
+
+        return Optional.empty();
+    }
+
+    public void delete(Long movieId){
+        repository.deleteById(movieId);
+    }
+
+    private List<Category> findCategories(List<Category> categories){
+        List<Category> categoriesFound = new ArrayList<>();
+        categories.forEach(category ->
+                categoryService.findById(category.getId()).ifPresent(categoriesFound::add));
+        return categoriesFound;
+    }
+
+    private List<Streaming> findStreamings(List<Streaming> streamings){
+        List<Streaming> streamingsFound = new ArrayList<>();
+        streamings.forEach(streaming ->
+                streamingService.findById(streaming.getId()).ifPresent(streamingsFound::add));
+        return streamingsFound;
+    }
+
+}
