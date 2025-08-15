4184404bd1ecc212acbb2897a59fbab70040d47b
dvlns <davi.carnauba@gmail.com>
2025-08-15 09:23:09 -0300

    feat: add user entity with authentication and login and registration endpoints

---
+++ src/main/java/com/movieflix/service/UserService.java
@@ -0,0 +1,20 @@
+package com.movieflix.service;
+
+import com.movieflix.entity.User;
+import com.movieflix.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+
+    public User save(User user){
+        String password = user.getPassword();
+        user.setPassword(passwordEncoder.encode(password));
+        return repository.save(user);
+    }
+}
