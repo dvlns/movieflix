799b26b677e4d9b762136e6eb5caf41ebe1b0034
dvlns <davi.carnauba@gmail.com>
2025-08-20 11:02:51 -0300

    feat: implement JWT token authentication

--- src/main/java/com/movieflix/config/TokenService.java
+++ src/main/java/com/movieflix/config/TokenService.java
@@ -2,11 +2,14 @@ package com.movieflix.config;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.movieflix.entity.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
+import java.util.Optional;
 
 @Component
 public class TokenService {
@@ -23,5 +26,23 @@ public class TokenService {
                 .withIssuedAt(Instant.now())
                 .withIssuer("movieflix")
                 .sign(algorithm);
+    }
+
+    public Optional<JWTUserData> verifyToken(String token) {
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            DecodedJWT jwt = JWT.require(algorithm)
+                    .build()
+                    .verify(token);
+
+            return Optional.of(JWTUserData
+                    .builder()
+                    .id(jwt.getClaim("userId").asLong())
+                    .name(jwt.getClaim("name").asString())
+                    .email(jwt.getSubject())
+                    .build());
+        } catch (JWTVerificationException ex){
+            return Optional.empty();
+        }
     }
 }
